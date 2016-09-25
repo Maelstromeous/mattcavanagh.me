2400567aa7ac1ce4d76d571832bd66b879ddaa2a
Matt Cavanagh <maelstrome26@gmail.com>
2016-09-25 18:41:09 +0100

    Added Javascript global file and added GA

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,8 @@ module.exports = function(grunt) {
             js: {
                 src: [
                     'bower_components/jquery/dist/jquery.min.js',
-                    'bower_components/bootstrap/dist/js/bootstrap.min.js'
+                    'bower_components/bootstrap/dist/js/bootstrap.min.js',
+                    'public/assets/js/global.js'
                 ],
                 dest: 'public/assets/dist/main.min.js'
             }
